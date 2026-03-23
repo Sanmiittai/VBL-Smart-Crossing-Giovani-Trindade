a8514de4197901e953d29d1d84036439fbb651a2
Sanmiittai <santhedev@gmail.com>
2026-03-23 14:29:21 -0300

    Added API Manager and Event Manager

---
+++ VBL-Smart-Crossing-Giovani-Trindade/Assets/Scripts/Managers/APIManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class APIManager : MonoBehaviour
+{
+    string apiURL = "http://localhost:3001/v1/traffic/status";
+    string apiReset = "http://localhost:3001/mockoon-admin/state/purge";
+
+    void OnEnable()
+    {
+        EventManager.AddListener(EventType.LevelAdvance, NewAPICall);
+        EventManager.AddListener(EventType.ResetAPIState, CallResetAPIState);
+    }
+
+    void OnDisable()
+    {
+        EventManager.RemoveListener(EventType.LevelAdvance, NewAPICall);
+        EventManager.RemoveListener(EventType.ResetAPIState, CallResetAPIState);
+    }
+
+    void Start()
+    {
+        StartCoroutine(ResetAPIState(true));
+    }
+
+    void NewAPICall(object data = null)
+    {
+        StartCoroutine(FetchTrafficStatus());
+    }
+
+    void CallResetAPIState(object data = null)
+    {
+        StartCoroutine(ResetAPIState(false));
+    }
+
+    IEnumerator ResetAPIState(bool gameStart)
+    {
+        using (UnityWebRequest request = UnityWebRequest.PostWwwForm(apiReset, ""))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"[API] Erros: {request.error}");
+            }
+            else if (gameStart)
+            {
+                NewAPICall();
+                NewAPICall();
+            }
+        }
+    }
+
+    IEnumerator FetchTrafficStatus()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(apiURL))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"[API] Error: {request.error}");
+            }
+            else
+            {
+                string jsonResponse = request.downloadHandler.text;
+                ProcessAPIResponse(jsonResponse);
+            }
+        }
+    }
+
+    void ProcessAPIResponse(string jsonResponse)
+    {
+        TrafficResponse data = JsonUtility.FromJson<TrafficResponse>(jsonResponse);
+
+        if (data != null)
+        {
+            EventManager.InvokeEvent(EventType.StatusChanged, data);
+        }
+        else
+        {
+            Debug.LogError("Data null");
+        }
+    }
+}
+
+[System.Serializable]
+public class Status
+{
+    public float vehicleDensity;
+    public float averageSpeed;
+    public string weather;
+}
+
+[System.Serializable]
+public class PredictedStatusItem
+{
+    public int estimated_time;
+    public Status prediction;
+}
+
+[System.Serializable]
+public class TrafficResponse
+{
+    public Status current_status;
+    public PredictedStatusItem[] predicted_status;
+}
---
+++ VBL-Smart-Crossing-Giovani-Trindade/Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    private static Dictionary<EventType, Action<object>> eventDictionary =
+        new Dictionary<EventType, Action<object>>();
+
+    public static void AddListener(EventType eventType, Action<object> listener)
+    {
+        if (!eventDictionary.ContainsKey(eventType))
+            eventDictionary[eventType] = listener;
+        else
+            eventDictionary[eventType] += listener;
+    }
+
+    public static void RemoveListener(EventType eventType, Action<object> listener)
+    {
+        if (eventDictionary.ContainsKey(eventType))
+            eventDictionary[eventType] -= listener;
+    }
+
+    public static void InvokeEvent(EventType eventType, object data = null)
+    {
+        if (eventDictionary.TryGetValue(eventType, out var thisEvent))
+            thisEvent?.Invoke(data);
+    }
+}
+public enum EventType
+{
+    StatusChanged,
+    ResetAPIState,
+    LevelAdvance
+}
