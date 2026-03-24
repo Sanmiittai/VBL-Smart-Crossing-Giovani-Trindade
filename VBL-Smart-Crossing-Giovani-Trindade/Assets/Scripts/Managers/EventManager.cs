993c8e91f4556da9c1c6b8a2bc85190ec7128dd8
Sanmiittai <santhedev@gmail.com>
2026-03-24 01:41:14 -0300

    Update EventManager.cs

--- VBL-Smart-Crossing-Giovani-Trindade/Assets/Scripts/Managers/EventManager.cs
+++ VBL-Smart-Crossing-Giovani-Trindade/Assets/Scripts/Managers/EventManager.cs
@@ -7,6 +7,7 @@ public class EventManager : MonoBehaviour
     private static Dictionary<EventType, Action<object>> eventDictionary =
         new Dictionary<EventType, Action<object>>();
 
+    //Adds the listener function to the specific event type
     public static void AddListener(EventType eventType, Action<object> listener)
     {
         if (!eventDictionary.ContainsKey(eventType))
@@ -15,12 +16,14 @@ public class EventManager : MonoBehaviour
             eventDictionary[eventType] += listener;
     }
 
+    //Removes the listener function from the specific event type
     public static void RemoveListener(EventType eventType, Action<object> listener)
     {
         if (eventDictionary.ContainsKey(eventType))
             eventDictionary[eventType] -= listener;
     }
 
+    //Calls all functions registered in the event type passed
     public static void InvokeEvent(EventType eventType, object data = null)
     {
         if (eventDictionary.TryGetValue(eventType, out var thisEvent))
